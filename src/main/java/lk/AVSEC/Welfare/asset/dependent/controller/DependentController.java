38d3025a6885a39d335a23579c46162f094b8bd7
LalithK90 <asakahatapitiya@gmail.com>
2020-10-12 21:57:22 +0530

    Dependent add issue was fixed

--- src/main/java/lk/AVSEC/Welfare/asset/dependent/controller/DependentController.java
+++ src/main/java/lk/AVSEC/Welfare/asset/dependent/controller/DependentController.java
@@ -11,9 +11,7 @@ import lk.AVSEC.Welfare.asset.employee.entity.Employee;
 import lk.AVSEC.Welfare.asset.employee.entity.Enum.EmployeeStatus;
 import lk.AVSEC.Welfare.asset.employee.service.EmployeeService;
 import lk.AVSEC.Welfare.asset.userManagement.service.UserService;
-import lk.AVSEC.Welfare.util.interfaces.AbstractController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -27,131 +25,135 @@ import java.util.stream.Collectors;
 @RequestMapping( "/dependent" )
 public class DependentController {
 
-    private final DependentService dependentService;
-    private final DependentEmployeeService dependentEmployeeService;
-    private final UserService userService;
-    private final EmployeeService employeeService;
+  private final DependentService dependentService;
+  private final DependentEmployeeService dependentEmployeeService;
+  private final UserService userService;
+  private final EmployeeService employeeService;
 
 
-    @Autowired
-    public DependentController(DependentService dependentService, DependentEmployeeService dependentEmployeeService,
-                               UserService userService, EmployeeService employeeService) {
-        this.dependentService = dependentService;
-        this.dependentEmployeeService = dependentEmployeeService;
-        this.userService = userService;
-        this.employeeService = employeeService;
+  @Autowired
+  public DependentController(DependentService dependentService, DependentEmployeeService dependentEmployeeService,
+                             UserService userService, EmployeeService employeeService) {
+    this.dependentService = dependentService;
+    this.dependentEmployeeService = dependentEmployeeService;
+    this.userService = userService;
+    this.employeeService = employeeService;
+  }
+
+  private String commonThing(Model model, Boolean booleanValue, Dependent dependentObject) {
+    model.addAttribute("relationship", Relationship.values());
+    model.addAttribute("currentStatus", CurrentStatus.values());
+    //  model.addAttribute("dependents", Province.values());
+    model.addAttribute("addStatus", booleanValue);
+    model.addAttribute("dependent", dependentObject);
+
+    return "dependent/addDependent";
+  }
+
+  @GetMapping
+  public String findAll(Model model) {
+    model.addAttribute("dependents", dependentEmployeeService.findAll()
+        .stream()
+        .distinct()
+        .collect(Collectors.toList()));
+    return "dependent/dependent";
+  }
+
+  @GetMapping( "/add/{id}" )
+  public String form(@PathVariable Integer id, Model model) {
+    model.addAttribute("employee", employeeService.findById(id));
+    return commonThing(model, false, new Dependent());
+  }
+
+  @GetMapping( "/{id}" )
+  public String findById(@PathVariable Integer id, Model model) {
+    model.addAttribute("dependentDetail", dependentEmployeeService.findById(id));
+    return "dependent/dependent-detail";
+  }
+
+  @GetMapping( "/edit/{id}" )
+  public String edit(@PathVariable Integer id, Model model) {
+    Dependent dependent = dependentService.findById(id);
+    Employee employee = dependent.getDependentEmployees().get(0).getEmployeeOne();
+    return commonThing(model, true, dependentEmployeeService.findByDependentAndEmployee(dependent, employee));
+  }
+
+  @PostMapping( value = {"/save", "/update"} )
+  public String persist(@Valid @ModelAttribute Dependent dependent, BindingResult bindingResult,
+                        RedirectAttributes redirectAttributes, Model model) {
+    Employee employee = employeeService.findById(dependent.getEmployee().getId());
+
+    if ( bindingResult.hasErrors() ) {
+      return commonThing(model, false, dependent);
     }
 
-    private String commonThing(Model model, Boolean booleanValue, Dependent dependentObject) {
-        model.addAttribute("relationship", Relationship.values());
-        model.addAttribute("currentStatus", CurrentStatus.values());
-        //  model.addAttribute("dependents", Province.values());
-        model.addAttribute("addStatus", booleanValue);
-        model.addAttribute("dependent", dependentObject);
-
-        return "dependent/addDependent";
+    //if there is registered dependent on system
+    if ( !dependent.getNic().isEmpty() ) {
+      System.out.println("existing dependent come to modified");
+      Dependent saveDependant;
+      Dependent dependentDb = dependentService.findByNic(dependent.getNic());
+      if ( dependentDb == null ) {
+        saveDependant = dependentService.persist(dependent);
+      } else {
+        saveDependant = dependentDb;
+      }
+      redirectAttributes.addFlashAttribute("dependentDetail", saveDependant);
+      return "redirect:/dependent";
     }
 
-    @GetMapping
-    public String findAll(Model model) {
-        model.addAttribute("dependents", dependentEmployeeService.findAll()
-                .stream()
-                .distinct()
-                .collect(Collectors.toList()));
-        return "dependent/dependent";
+    // create a new dependentEmployee and set value to it
+    DependentEmployee dependentEmployee = new DependentEmployee();
+
+
+    //relationship if wife husband -> employeeOne or employeeTwo
+    if ( dependent.getRelationship().equals(Relationship.HUS) || dependent.getRelationship().equals(Relationship.WIF) && !dependent.getEpfNumber().isEmpty() ) {
+      System.out.println("Dependent employee is AVSEC");
+      Employee employeeDB = employeeService.findByEpf(dependent.getEpfNumber());
+      if ( employeeDB.equals(employee) ) {
+          redirectAttributes.addFlashAttribute("message", "Are you kidding me ? \n Do not try to add employee as depender on same employee...");
+
+          return "redirect:/dependent";
+      }
+      //husband or wife
+      dependentEmployee.setEmployeeTwo(employeeDB);
+      //set relation ship
+      dependentEmployee.setRelationship(dependent.getRelationship());
+      //working employee
+      dependentEmployee.setEmployeeOne(employee);
+
+      //make new dependent using employee
+      Dependent dependentAVSEC = new Dependent();
+      //set employee status
+      if ( employeeDB.getEmployeeStatus().equals(EmployeeStatus.RESIGNED) ) {
+        dependentAVSEC.setCurrentStatus(CurrentStatus.OTR);
+      } else {
+        dependentAVSEC.setCurrentStatus(CurrentStatus.ACT);
+      }
+      dependentAVSEC.setDob(employeeDB.getDateOfBirth());
+      dependentAVSEC.setName(employeeDB.getName());
+      dependentAVSEC.setNic(employeeDB.getNic());
+      dependentAVSEC.setInsideOrOut(InsideOrOut.IN);
+
+      //created dependent set to dependent employee
+      dependentEmployee.setDependent(dependentAVSEC);
+
+    } else {
+      dependentEmployee.setRelationship(dependent.getRelationship());
+      dependentEmployee.setEmployeeOne(employee);
+      dependent.setInsideOrOut(InsideOrOut.OUT);
+      dependentEmployee.setDependent(dependent);
     }
 
-    @GetMapping( "/add/{id}" )
-    public String form(@PathVariable Integer id, Model model) {
-        model.addAttribute("employee", employeeService.findById(id));
-        return commonThing(model, false, new Dependent());
-    }
 
-    @GetMapping( "/{id}" )
-    public String findById(@PathVariable Integer id, Model model) {
-        model.addAttribute("dependentDetail", dependentEmployeeService.findById(id));
-        return "dependent/dependent-detail";
-    }
+    redirectAttributes.addFlashAttribute("dependentDetail", dependentEmployeeService.persist(dependentEmployee));
+    return "redirect:/dependent";
+  }
 
-    @GetMapping( "/edit/{id}" )
-    public String edit(@PathVariable Integer id, Model model) {
-        Dependent dependent = dependentService.findById(id);
-        Employee employee = dependent.getDependentEmployees().get(0).getEmployeeOne();
-        return commonThing(model, true, dependentEmployeeService.findByDependentAndEmployee(dependent, employee));
-    }
-
-    @PostMapping( value = {"/save", "/update"} )
-    public String persist(@Valid @ModelAttribute Dependent dependent, BindingResult bindingResult,
-                          RedirectAttributes redirectAttributes, Model model) {
-        Employee employee = employeeService.findById(dependent.getEmployee().getId());
-
-        if ( bindingResult.hasErrors() ) {
-            return commonThing(model, false, dependent);
-        }
-
-        //if there is registered dependent on system
-        if ( !dependent.getNic().isEmpty() ) {
-            System.out.println("existing dependent come to modified");
-            Dependent saveDependant;
-            Dependent dependentDb = dependentService.findByNic(dependent.getNic());
-            if ( dependentDb == null ) {
-                saveDependant = dependentService.persist(dependent);
-            } else {
-                saveDependant = dependentDb;
-            }
-            redirectAttributes.addFlashAttribute("dependentDetail", saveDependant);
-            return "redirect:/dependent";
-        }
-
-        // create a new dependentEmployee and set value to it
-        DependentEmployee dependentEmployee = new DependentEmployee();
-
-
-        //relationship if wife husband -> employeeOne or employeeTwo
-        if ( dependent.getRelationship().equals(Relationship.HUS) || dependent.getRelationship().equals(Relationship.WIF) && !dependent.getEpfNumber().isEmpty() ) {
-            System.out.println("Dependent employee is AVSEC");
-            Employee employeeDB = employeeService.findByEpf(dependent.getEpfNumber());
-
-            //husband or wife
-            dependentEmployee.setEmployeeTwo(employeeDB);
-            //set relation ship
-            dependentEmployee.setRelationship(dependent.getRelationship());
-            //working employee
-            dependentEmployee.setEmployeeOne(employee);
-
-            //make new dependent using employee
-            Dependent dependentAVSEC = new Dependent();
-            //set employee status
-            if ( employeeDB.getEmployeeStatus().equals(EmployeeStatus.RESIGNED) ) {
-                dependentAVSEC.setCurrentStatus(CurrentStatus.OTR);
-            } else {
-                dependentAVSEC.setCurrentStatus(CurrentStatus.ACT);
-            }
-            dependentAVSEC.setDob(employeeDB.getDateOfBirth());
-            dependentAVSEC.setName(employeeDB.getName());
-            dependentAVSEC.setNic(employeeDB.getNic());
-            dependentAVSEC.setInsideOrOut(InsideOrOut.IN);
-
-            //created dependent set to dependent employee
-            dependentEmployee.setDependent(dependentAVSEC);
-
-        } else {
-            dependentEmployee.setRelationship(dependent.getRelationship());
-            dependentEmployee.setEmployeeOne(employee);
-            dependent.setInsideOrOut(InsideOrOut.OUT);
-            dependentEmployee.setDependent(dependent);
-        }
-
-
-        redirectAttributes.addFlashAttribute("dependentDetail", dependentEmployeeService.persist(dependentEmployee));
-        return "redirect:/dependent";
-    }
-
-    @GetMapping( "/delete/{id}" )
-    public String delete(@PathVariable Integer id, Model model) {
-        dependentService.delete(id);
-        return "redirect:/dependent";
-    }
+  @GetMapping( "/delete/{id}" )
+  public String delete(@PathVariable Integer id, Model model) {
+    dependentService.delete(id);
+    return "redirect:/dependent";
+  }
 
 
 }
