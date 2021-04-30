ca3d38a45acec074a674634e63d6022a6c8f8679
WB Prasad <airportprasad@gmail.com>
2021-04-30 13:43:56 +0530

    mytable not working was fixed

--- src/main/resources/static/js/myJs.js
+++ src/main/resources/static/js/myJs.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
 
 
     /*//--------------- data table short using - data table plugin ------- start //*/
-    if ($("#myTable").val()) {
+    if ($("#myTable tr").length) {
         $("#myTable").DataTable({
             "lengthMenu": [[5, 10, 15, 20, -1], [5, 10, 15, 20, "All"]],
             "ordering": false,
